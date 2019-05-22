04f6dba5bc0fb112ba7065817ab8b786637f415d
Rainer Sawitzki <training@rainer-sawitzki.de>
2019-05-22 12:18:01 +0200

    introduce recorder and player

--- org.javacream.training.testdriven/src/test/java/org/javacream/books/warehouse/api/scenario/CreateAndFindBookTest.java
+++ org.javacream.training.testdriven/src/test/java/org/javacream/books/warehouse/api/scenario/CreateAndFindBookTest.java
@@ -8,6 +8,9 @@ import org.javacream.books.warehouse.api.BooksService;
 import org.javacream.books.warehouse.api.IsbnGenerator;
 import org.javacream.books.warehouse.api.StoreService;
 import org.javacream.books.warehouse.business.MapBooksService;
+import org.javacream.books.warehouse.business.PropertiesStoreService;
+import org.javacream.util.test.XmlPlayer;
+import org.javacream.util.test.XmlRecordingDecorator;
 import org.javacream.util.testdriver.GenericDummy;
 import org.junit.Assert;
 import org.junit.Test;
@@ -37,6 +40,36 @@ public class CreateAndFindBookTest {
 		Book result = booksService.findBookByIsbn(isbn);
 		Assert.assertEquals(isbn, result.getIsbn());
 		Assert.assertEquals(TITLE, result.getTitle());
+	}
+	@Test 
+	public void createdBookCanBeFoundWithRecorder(){
+		MapBooksService booksService = new MapBooksService();
+		booksService.setBooks(new HashMap<>());
+		IsbnGenerator isbnGenerator = GenericDummy.createDummy(IsbnGenerator.class);
+		booksService.setIsbnGenerator(isbnGenerator);
+		StoreService storeService = new PropertiesStoreService("books");
+		storeService = XmlRecordingDecorator.decorate(storeService, "c:/_training/storeinvocations.xml");
+		booksService.setStoreService(storeService);;
+		final String TITLE = "TITLE";
+		String isbn = booksService.newBook(TITLE);
+		Book result = booksService.findBookByIsbn(isbn);
+		Assert.assertEquals(isbn, result.getIsbn());
+		Assert.assertEquals(TITLE, result.getTitle());
+	}
+
+	@Test public void createdBookCanBeFoundWithPlayer(){
+		MapBooksService booksService = new MapBooksService();
+		booksService.setBooks(new HashMap<>());
+		IsbnGenerator isbnGenerator = GenericDummy.createDummy(IsbnGenerator.class);
+		booksService.setIsbnGenerator(isbnGenerator);
+		StoreService storeService = XmlPlayer.createPlayer(StoreService.class, "c:/_training/storeinvocations.xml");
+		booksService.setStoreService(storeService);;
+		final String TITLE = "TITLE";
+		String isbn = booksService.newBook(TITLE);
+		Book result = booksService.findBookByIsbn(isbn);
+		Assert.assertEquals(isbn, result.getIsbn());
+		Assert.assertEquals(TITLE, result.getTitle());
+		System.out.println(result.isAvailable());
 	}
 
 }
