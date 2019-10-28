f4123a524fe80c2aeef044f5641cb58571271641
Rainer Sawitzki <training@rainer-sawitzki.de>
2019-10-28 15:18:00 +0100

    new Operations for BooksService

--- org.javacream.training.testdriven/src/org/javacream/books/warehouse/api/BooksService.java
+++ org.javacream.training.testdriven/src/org/javacream/books/warehouse/api/BooksService.java
@@ -1,9 +1,19 @@
 package org.javacream.books.warehouse.api;
 
+import java.util.Map;
+
+import org.javacream.util.test.SpecifiedBy;
+
 public interface BooksService {
 
 	String newBook(String title);
 
 	Book findBookByIsbn(String isbn);
+
+	@SpecifiedBy(url = "LöscheBuch.txt")
+	void deleteBookByIsbn(String isbn);
+
+	@SpecifiedBy(url = "updateBook.jpg")
+	void updateBook(String isbn, Map<String, Object> options);
 
 }
