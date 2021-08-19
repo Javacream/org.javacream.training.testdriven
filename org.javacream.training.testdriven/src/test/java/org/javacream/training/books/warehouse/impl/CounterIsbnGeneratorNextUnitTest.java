3df4ab5685033d2499652a046b3add99361f3884
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-08-19 11:24:23 +0200

    fix package for CounterIsbnGeneratorNextUnitTest

--- org.javacream.training.testdriven/src/test/java/org/javacream/training/books/warehouse/impl/CounterIsbnGeneratorNextUnitTest.java
+++ org.javacream.training.testdriven/src/test/java/org/javacream/training/books/warehouse/impl/CounterIsbnGeneratorNextUnitTest.java
@@ -1,4 +1,4 @@
-package org.javacream.training.books.warehouse.api;
+package org.javacream.training.books.warehouse.impl;
 
 import org.javacream.books.warehouse.impl.CounterIsbnGenerator;
 import org.junit.Assert;
