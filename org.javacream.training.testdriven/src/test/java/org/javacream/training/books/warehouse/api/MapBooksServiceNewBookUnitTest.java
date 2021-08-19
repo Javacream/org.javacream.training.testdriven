e2f7b3e4c8145f249f848774835d173314b637b3
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-08-19 14:54:48 +0200

    add mocking class example

---
+++ org.javacream.training.testdriven/src/test/java/org/javacream/training/books/warehouse/api/MapBooksServiceNewBookUnitTest.java
@@ -0,0 +1,37 @@
+package org.javacream.training.books.warehouse.api;
+
+import java.util.HashMap;
+
+import org.javacream.books.warehouse.api.Book;
+import org.javacream.books.warehouse.impl.CounterIsbnGenerator;
+import org.javacream.books.warehouse.impl.MapBooksService;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class MapBooksServiceNewBookUnitTest {
+
+	private MapBooksService booksService;
+	private static final String ISBN = "ISBN42";
+	@Before public void init() {
+		booksService = new MapBooksService();
+		booksService.setBooks(new HashMap<String, Book>());
+
+		CounterIsbnGenerator isbnGenerator = Mockito.mock(CounterIsbnGenerator.class);
+		Mockito.when(isbnGenerator.nextIsbn()).thenReturn(ISBN);
+		booksService.setIsbnGenerator(isbnGenerator);
+	}
+	
+	@Test public void anNonEmptyTitleCreatesIsbn() {
+		String isbn = booksService.newBook("NEW");
+		Assert.assertEquals(ISBN, isbn);
+	}
+
+	@Test(expected=IllegalArgumentException.class) public void nullTitleThrowsIllegalArgument() {
+		booksService.newBook(null);
+	}
+	@Test(expected=IllegalArgumentException.class) public void emptyTitleThrowsIllegalArgument() {
+		booksService.newBook("");
+	}
+}
