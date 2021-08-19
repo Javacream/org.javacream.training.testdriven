e7181a4656dc22aa3cadd988b20ca1ba9a60b18d
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-08-19 11:13:53 +0200

    fix StoreService, add "SpecifiedBy

--- org.javacream.training.testdriven/src/main/java/org/javacream/store/api/StoreService.java
+++ org.javacream.training.testdriven/src/main/java/org/javacream/store/api/StoreService.java
@@ -1,5 +1,8 @@
 package org.javacream.store.api;
 
+import org.javacream.test.SpecifiedBy;
+
+@SpecifiedBy(url="https://github.com/Javacream/org.javacream.training.testdriven/blob/integrata_18.8.2021/specs/storeservice.txt")
 public interface StoreService {
 
 	int getStock(String category, String id);
--- org.javacream.training.testdriven/src/main/java/org/javacream/store/impl/SimpleStoreService.java
+++ org.javacream.training.testdriven/src/main/java/org/javacream/store/impl/SimpleStoreService.java
@@ -5,7 +5,9 @@ import java.util.List;
 import java.util.Properties;
 
 import org.javacream.store.api.StoreService;
+import org.javacream.test.SpecifiedBy;
 
+@SpecifiedBy(url="https://github.com/Javacream/org.javacream.training.testdriven/blob/integrata_18.8.2021/specs/storeservice.txt")
 public class SimpleStoreService implements StoreService {
 
 	private HashMap<String, Properties> store;
