4430485b40c124a183d08e97455ed29d5b2cbe45
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2021-08-20 10:02:26 +0200

    Update OrderIdGenerator.java

--- org.javacream.training.testdriven/src/main/java/org/javacream/books/order/impl/OrderIdGenerator.java
+++ org.javacream.training.testdriven/src/main/java/org/javacream/books/order/impl/OrderIdGenerator.java
@@ -1,5 +1,14 @@
 package org.javacream.books.order.impl;
 
-public class OrderIdGenerator {
+import java.util.Random;
 
+public class OrderIdGenerator {
+	private Random random;
+	
+	public OrderIdGenerator(long seed){
+		random = new Random(seed);
+	}
+	public long nextId() {
+		return random.nextLong();
+	}
 }
